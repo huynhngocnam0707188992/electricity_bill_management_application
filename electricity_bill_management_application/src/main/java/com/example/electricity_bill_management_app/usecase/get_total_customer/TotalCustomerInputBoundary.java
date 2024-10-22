47de2ccf9647cfd0ae7d4ee646243303ede9518b
huynhngocnam0707188992 <huynhngocnam027@gmail.com>
2024-10-22 14:56:13 +0700

    update lan 4

--- electricity_bill_management_application/src/main/java/com/example/electricity_bill_management_app/usecase/get_total_customer/TotalCustomerInputBoundary.java
+++ electricity_bill_management_application/src/main/java/com/example/electricity_bill_management_app/usecase/get_total_customer/TotalCustomerInputBoundary.java
@@ -1,4 +1,4 @@
-package com.example.electricity_bill_management_app.usecase;
+package com.example.electricity_bill_management_app.usecase.get_total_customer;
 
 public interface TotalCustomerInputBoundary {
 
