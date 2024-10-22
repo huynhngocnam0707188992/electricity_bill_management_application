c10e33cd4a4409169b1889ddd7e983a269071001
huynhngocnam0707188992 <huynhngocnam027@gmail.com>
2024-10-22 16:02:30 +0700

    update lan 5

--- electricity_bill_management_application/src/main/java/com/example/electricity_bill_management_app/entity/ElectricBill.java
+++ electricity_bill_management_application/src/main/java/com/example/electricity_bill_management_app/entity/ElectricBill.java
@@ -3,6 +3,7 @@ package com.example.electricity_bill_management_app.entity;
 import java.util.Date;
 
 
+
 public abstract class ElectricBill {
 
     protected String billID;
@@ -11,9 +12,10 @@ public abstract class ElectricBill {
     protected float totalPrice;
     protected Date billDate;
     protected String customerName;
-    
+
     public ElectricBill() {
     }
+
     public ElectricBill(String billID, int quantity, float unitPrice, Date billDate,
             String customerName) {
         this.billID = billID;
@@ -23,44 +25,46 @@ public abstract class ElectricBill {
         this.customerName = customerName;
     }
 
-    protected void execute (){
-
-    }
-
-
+    abstract public float calculated();
 
     public String getBillID() {
         return billID;
     }
+
     public void setBillID(String billID) {
         this.billID = billID;
     }
+
     public int getQuantity() {
         return quantity;
     }
+
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
+
     public float getUnitPrice() {
         return unitPrice;
     }
+
     public void setUnitPrice(float unitPrice) {
         this.unitPrice = unitPrice;
     }
-   
+
     public Date getBillDate() {
         return billDate;
     }
+
     public void setBillDate(Date billDate) {
         this.billDate = billDate;
     }
+
     public String getCustomerName() {
         return customerName;
     }
+
     public void setCustomerName(String customerName) {
         this.customerName = customerName;
     }
-
-    
 
 }
